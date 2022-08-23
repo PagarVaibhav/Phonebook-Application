80c8504d0896045366887402e6b1c34106405e67
PagarVaibhav <vaibhavpagar99@gmail.com>
2022-08-23 17:46:49 +0530

    PA-3 Create Entity Class with Required Field And Contact Repository

--- src/main/java/com/BikkadIT/Controller/ContactController.java
+++ src/main/java/com/BikkadIT/Controller/ContactController.java
@@ -1,5 +1,8 @@
 package com.BikkadIT.Controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ContactController {
 
 }
--- src/main/java/com/BikkadIT/ServiceImpl/ContactServiceImpl.java
+++ src/main/java/com/BikkadIT/ServiceImpl/ContactServiceImpl.java
@@ -1,5 +1,8 @@
 package com.BikkadIT.ServiceImpl;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class ContactServiceImpl {
 
 }
